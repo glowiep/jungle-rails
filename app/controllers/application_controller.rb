e6f86b8547063ae9f0c21d00daae134f35dff7dc
Gloria Lim <glorialimartt@gmail.com>
2024-03-13 10:08:26 -0400

    Add sign up and log in page. Add  logic to display user logged in status in the top nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,20 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  def authorize
+    redirect_to '/login' unless logged_in?
+  end
+  
+
   private
 
   def cart
