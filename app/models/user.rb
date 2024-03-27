fefc578c68d9680954a4cd413958723495f62acf
Gloria Lim <glorialimartt@gmail.com>
2024-03-26 22:11:10 -0400

    Add tests for User. Add a custom password validation method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,23 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { in: 7..20 }
-  validates :password_confirmation, presence: true
-  
   def self.authenticate_with_credentials(email, password)
     email = email.strip.downcase
     user = find_by_email(email)
     user if user&.authenticate(password)
   end
+
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { in: 7..20 }
+  validates :password_confirmation, presence: true  
+
+  private
+
+  def password_and_confirmation_match
+    if password != password_confirmation
+      errors.add(:password_confirmation, "must match the password")      
+    end    
+  end  
 end  
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,9 +72,83 @@ RSpec.describe User, type: :model do
       )
       @user.save
       expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end  
+    
+    it 'must have a password greater than 6 characters' do
+      @user = User.new(
+        first_name: "Gloria",
+        last_name: "Lim",
+        email: "glim@test.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      @user.save
+      expect(@user).to_not be_valid
+      # expect(@user.password.size).to be = 3
+    end
+
+    it 'emails must be unique' do
+      @user1 = User.new(
+        first_name: "Gloria",
+        last_name: "Lim",
+        email: "glim@test.com",
+        password: "1234567",
+        password_confirmation: "1234567"
+      )
+      @user1.save
+      @user2 = User.new(
+        first_name: "Gloria",
+        last_name: "Lim",
+        email: "glim@test.com",
+        password: "1234567",
+        password_confirmation: "1234567"
+      )
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+    
+    it 'will not accept passwords that do not match' do
+      @user = User.new(
+        first_name: "Gloria",
+        last_name: "Lim",
+        email: "glim@test.com",
+        password: "1234567",
+        password_confirmation: "1234567i"
+      )
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
     
     
   end
+
+  describe 'authenticate _with_credentials' do
+    it 'will not authenticate an email that does not exist' do
+      @user = User.new(
+        first_name: "Gloria",
+        last_name: "Lim",
+        email: " glim@test.com  ",
+        password: "1234567",
+        password_confirmation: "1234567"
+      )
+      authenticated_user = User.authenticate_with_credentials('new@test.com', "password")
+      expect(authenticated_user).to be nil
+    end
+
+    it 'will not authenticate incorrect passwords' do
+      # expect().
+    end
+
+    it 'will authenticate correctly spelled emails with wrong case' do
+      # expect().
+    end
+    
+    it 'will return the user if email and password match' do
+      # expect().
+    end
+    
+    
+  end
+  
   
 end
