9849349de66c6f4d77a402087577ccb5b1041f41
Gloria Lim <glorialimartt@gmail.com>
2024-03-27 14:14:57 -0400

    Add cypress tests for home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,3 +1,13 @@
-describe('Visit home page', () => {
-  cy.visit('http://localhost:3000/')
+describe('Home page', () => {
+  it("should visit the home page", () => {
+    cy.visit('/');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
